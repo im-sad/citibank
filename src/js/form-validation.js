3f4a09ea338211cafeed0df8eba1d83e403fd86e
Andrew <im-sad@ya.ru>
2020-05-11 16:35:18 +0300

    Remove duplicate

--- src/js/form-validation.js
+++ src/js/form-validation.js
@@ -179,37 +179,4 @@ document.addEventListener('DOMContentLoaded', function() {
     }
   })();
 
-
-  // Modals
-  (function() {
-    initBaseModals();
-
-    function initBaseModals() {
-      var modals = document.getElementsByClassName('js-modal');
-
-      for (var i = 0; i < modals.length; i++) {
-        initModal(modals[i]);
-      }
-    }
-
-    function initModal(modalElement) {
-      var modalPlugin = new Modal(modalElement);
-      var modalId =  modalElement.getAttribute('id');
-
-      addButtonsEvent(modalPlugin);
-
-      return modalPlugin;
-
-      function addButtonsEvent(obj) {
-        var modalBtns = document.querySelectorAll('[data-modal="'+ modalId +'"]');
-
-        for (var j = 0; j < modalBtns.length; j++) {
-          modalBtns[j].addEventListener('click', function() {
-            obj.open();
-            }, false);
-        }
-      }
-    }
-  })();
-
 });
